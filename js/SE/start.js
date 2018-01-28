056b2b9491ecc1206266300ac1ff62062b96874b
lost273 <dnk273@gmail.com>
2018-01-28 18:23:21 +0300

    28-01

--- js/SE/start.js
+++ js/SE/start.js
@@ -1,11 +1,19 @@
-
-
-var el = document.querySelectorAll("#field.row div");
-for (var index = 0; index < el.length; index++) {
-    console.log(el[index].innerText);
-    el[index].addEventListener("click", move);
+/*
+var newElement = document.createElement('p');
+newElement.innerText = '123';
+el.appendChild(newElement);
+*/
+var parentContainer = document.getElementById('body-content');
+var elementsToRemove = parentContainer.getElementsByTagName('*');
+var n = elementsToRemove.length;
+for (var index = 0; index < n; index++) {
+    elementsToRemove.removeChild(elementsToRemove[index]);
+    //console.log(elementsToRemove[index]);
 }
+
+/*
 document.getElementById('game').addEventListener('click');
 document.getElementById('bubble').addEventListener('click');
 document.getElementById('insert').addEventListener('click');
 document.getElementById('select').addEventListener('click');
+*/
