7705f2cb89570c4476b30423e3bce7f8a6254367
lost273 <dnk273@gmail.com>
2018-01-28 13:43:09 +0300

    28-01

---
+++ js/SE/start.js
@@ -0,0 +1,5 @@
+var el = document.querySelectorAll("#field.row div");
+for (var index = 0; index < el.length; index++) {
+    console.log(el[index].innerText);
+    el[index].addEventListener("click", move);
+}
