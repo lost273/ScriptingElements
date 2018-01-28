b1b9358585f68af03fdef92dadce50a9147d4339
lost273 <dnk273@gmail.com>
2018-01-28 15:37:13 +0300

    28-01

--- js/SE/start.js
+++ js/SE/start.js
@@ -5,3 +5,7 @@ for (var index = 0; index < el.length; index++) {
     console.log(el[index].innerText);
     el[index].addEventListener("click", move);
 }
+document.getElementById('game').addEventListener('click');
+document.getElementById('bubble').addEventListener('click');
+document.getElementById('insert').addEventListener('click');
+document.getElementById('select').addEventListener('click');
