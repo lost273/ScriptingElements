8bb10378fa2a64c106861e00ecc3573d0ea41a29
lost273 <dnk273@gmail.com>
2018-01-29 17:50:44 +0300

    29-01

--- js/SE/start.js
+++ js/SE/start.js
@@ -11,10 +11,25 @@ function clearContainers(name){
 	    parentContainer.removeChild(elementsToRemove[0]);
 	}
 }
+//construct html tags for the game
+function makeGame(){
+	var parentContainer = document.getElementById('body-content');
+	var rowElement = document.createElement('div');
+	rowElement.className = "row";
+	rowElement.id = "field";
+	var colElement = document.createElement('div');
+	colElement.className = "col-md-1";
+	colElement.id = "square";
+	parentContainer.appendChild(rowElement);
+	rowElement.appendChild(colElement);
+	rowElement.appendChild(colElement);
+}
+
 clearContainers('body-content');
 clearContainers('scripts-block');
+makeGame();
 /*
-document.getElementById('game').addEventListener('click');
+document.getElementById('game').addEventListener('click', makeGame);
 document.getElementById('bubble').addEventListener('click');
 document.getElementById('insert').addEventListener('click');
 document.getElementById('select').addEventListener('click');
