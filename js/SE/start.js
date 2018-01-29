4c973f776e19000fe6f13dcdb74228d1c95e15da
lost273 <dnk273@gmail.com>
2018-01-29 17:18:39 +0300

    29-01

--- js/SE/start.js
+++ js/SE/start.js
@@ -3,12 +3,16 @@ var newElement = document.createElement('p');
 newElement.innerText = '123';
 el.appendChild(newElement);
 */
-var parentContainer = document.getElementById('body-content');
-var elementsToRemove = parentContainer.getElementsByTagName('*');
-while (elementsToRemove.length !== 0) {
-    parentContainer.removeChild(elementsToRemove[0]);
+//delete contents of the container
+function clearContainers(name){
+	var parentContainer = document.getElementById(name);
+	var elementsToRemove = parentContainer.getElementsByTagName('*');
+	while (elementsToRemove.length !== 0) {
+	    parentContainer.removeChild(elementsToRemove[0]);
+	}
 }
-
+clearContainers('body-content');
+clearContainers('scripts-block');
 /*
 document.getElementById('game').addEventListener('click');
 document.getElementById('bubble').addEventListener('click');
